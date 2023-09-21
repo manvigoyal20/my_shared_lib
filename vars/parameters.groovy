3424ae59c456f1b9e362219440841b168b6547d5
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-21 15:01:53 +0530

    added

--- vars/parameters.groovy
+++ vars/parameters.groovy
@@ -1,10 +1,10 @@
 def call(Map config){
-    parameterDefinitions([
+    properties([parameters([
 
     choice(name:'branch', choices:['main', 'develop'], description:'choose any branch')
     booleanParam(name: 'testParam', defaultValue: 'true', description:'select true or false')
 
-    ])
+    ])])
 
     node {
         echo "Select ${config.branch} and ${config.testParam} to run the Pipeline"
