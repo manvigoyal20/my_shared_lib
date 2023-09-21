ee5f7642de93c2b90b3329164bb340e86a78c7d4
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-21 11:47:17 +0530

    params sl

--- vars/parameters.groovy
+++ vars/parameters.groovy
@@ -1,6 +1,8 @@
-parameters{
+def call(Map config){
+    parameterDefinitions([
 
-    choice(name:'branch', choices:'main\ndevelop', description:'choose any branch')
+    choice(name:'branch', choices:['main', 'develop'], description:'choose any branch')
     booleanParam(name: 'testParam', defaultValue: 'true', description:'select true or false')
 
+    ])
 }
