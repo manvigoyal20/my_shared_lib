207e16f6036bcb05a0571a717d9585ee665e7839
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-21 15:54:38 +0530

    add

--- vars/customParameters.groovy
+++ vars/customParameters.groovy
@@ -1,8 +1,8 @@
 def call(Map config){
     properties([parameters([
 
-    choice(name:'branch', choices:['main', 'develop'], description:'choose any branch')
-    // booleanParam(name: 'testParam', defaultValue: 'true', description:'select true or false')
+    choice(name:'branch', choices:['main', 'develop'], description:'choose any branch'),
+    booleanParam(name: 'testParam', defaultValue: 'true', description:'select true or false')
 
     ])])    
 
