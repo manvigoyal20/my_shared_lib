22a0d017868270840bed70397ebdd498c1e37fcc
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-21 15:12:53 +0530

    test

--- vars/customParameters.groovy
+++ vars/customParameters.groovy
@@ -1,12 +1,15 @@
-def call(Map config){
-    properties([parameters([
-
-    choice(name:'branch', choices:['main', 'develop'], description:'choose any branch')
-    booleanParam(name: 'testParam', defaultValue: 'true', description:'select true or false')
-
-    ])])
-
-    node {
-        echo "Select ${config.branch} and ${config.testParam} to run the Pipeline"
-    }
+def call() {
+    properties([
+        parameters([
+            booleanParam(defaultValue: false, name: 'BuildAll', description: ''),
+            [$class: 'ChoiceParameter', choiceType: 'PT_CHECKBOX',
+                description: 'Choose environment category.',
+                name: 'ENVIRONMENT',
+                script: [
+                    $class: 'GroovyScript',
+                    script: [sandbox: true, script: 'return ["QA", "DEV", "PROD"]']
+                ]
+            ]
+        ])
+    ])
 }
