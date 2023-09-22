962e172fea3435266f289a8a578e88253bb0fadf
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-22 11:01:30 +0530

    docker params

--- vars/customParameters.groovy
+++ vars/customParameters.groovy
@@ -3,6 +3,9 @@ def call(Map config){
 
     choice(name:'branch', choices:['main', 'develop'], description:'choose any branch'),
     booleanParam(name: 'testParam', defaultValue: 'true', description:'select true or false')
+    string(name: 'imageName', defaultValue: 'javaapp', description:'name of the image')
+    string(name: 'imageTag', defaultValue: 'v1', description:'tag of the image')
+    string(name: 'dockerhubUser', defaultValue: 'manvigoyal', description:'user name of dockerhub')
 
     ])])    
 
