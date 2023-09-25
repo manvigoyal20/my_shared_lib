83d778fe23a376fac1e61fc1d1f6011d17c1e81c
manvigoyal20 <manvigoyal123@outlook.com>
2023-09-25 13:15:30 +0530

    commit

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String project, String imageTag, String hubUser){
     sh """
-    docker image build -t ${hubUser}:${project} .
+    docker build -t ${hubUser}:${project} .
     docker image tag ${hubUser}:${project} ${hubUser}:${project}:${imageTag}
     docker image tag ${hubUser}:${project} ${hubUser}:${project}:latest
     """
